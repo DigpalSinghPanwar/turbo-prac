d9c6ffa1df972ea78799f76f7f0d86ebbaa40e35
DigpalSinghPanwar784 <singh.digpal784@gmail.com>
2025-04-06 23:51:20 +0530

    first commit.

---
+++ apps/docs/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import { Admin } from "@repo/ui/admin";
+
+export default function Home() {
+  return (
+    <>
+      <h3>this is app</h3>
+      <Admin />
+    </>
+  );
+}
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export function Admin() {
+  return (
+    <>
+      <h2>This is admin page</h2>
+    </>
+  );
+}
